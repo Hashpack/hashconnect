d6e0816eb2d208398391f58c5f2e78b7f896d15e
teacoat <tyler8cote@gmail.com>
2021-11-06 14:45:42 -0400

    added lib3

--- example/dapp/src/app/app.component.ts
+++ example/dapp/src/app/app.component.ts
@@ -1,32 +1,10 @@
 import { Component } from '@angular/core';
-// import { initWaku, currentMessage, sendMessage } from 'hashconnect';
-import { test } from 'hashconnect2';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'dapp';
-    status = "not started"
-    message = ""
-    incomingMessage = "";
-
-    constructor() {
-        test("aaa");
-    //   console.log(initWaku);
-        // currentMessage.subscribe((msg) => this.incomingMessage += msg + "\n")
-    }
-
-    async initClient() {
-        // await initWaku();
-        this.status = "initialized"
-      }
-    
-      async send() {
-        this.status = "message sending..."
-        // await sendMessage(this.message);
-        this.status = "message sent"
-      }
+  title = 'dapp';
 }

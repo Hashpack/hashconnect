4382360b5c6fde8e734ec95f3d0c887630943897
teacoat <tyler8cote@gmail.com>
2021-11-06 15:19:14 -0400

    got it working

--- example/dapp/src/app/app.component.ts
+++ example/dapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { greet } from 'hashconnect';
 
 @Component({
   selector: 'app-root',
--- lib3/src/main.ts
+++ lib3/src/main.ts
@@ -1,6 +1,12 @@
+import {  Waku } from 'js-waku';
+
 export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
 
-export const greet = (name: string): string => `Hello ${name}`
+export const greet = async (name: string): Promise<string> => {
+    let waku = await Waku.create({ bootstrap: true });
+    console.log(waku);
+    return `Hello ${name}`
+}
 
 export const foo = async (): Promise<boolean> => {
   console.log(greet('World'))

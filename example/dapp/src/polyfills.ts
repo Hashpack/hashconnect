d2da442997c9d746f6ed2f4fbe6345b310446bdf
nick <nick@hashpack.app>
2021-11-06 10:12:55 -0400

    a little further

--- example/dapp/src/polyfills.ts
+++ example/dapp/src/polyfills.ts
@@ -58,8 +58,8 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
-// (window as any).global = window;
-// global.Buffer = global.Buffer || require('buffer').Buffer;
+(window as any).global = window;
+global.Buffer = global.Buffer || require('buffer').Buffer;
 global.process = require('process');
 
 /***************************************************************************************************

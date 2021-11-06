fc77f429d1dac73b8deca1eefb43429d691d4cb7
nick <nick@hashpack.app>
2021-11-06 17:24:44 -0400

    baton pass

--- lib/src/main.ts
+++ lib/src/main.ts
@@ -1,46 +1,49 @@
-import { Waku } from 'js-waku';
+import { WakuMessage, Waku, getBootstrapNodes } from 'js-waku';
+import { Event } from "ts-typed-events"
 
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+const protons = require('protons');
 
-export const greet = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
+let ContentTopic = "/waku/2/default-waku/proto"
+const proto = protons(`
+message SimpleChatMessage {
+  uint64 timestamp = 1;
+  string text = 2;
+}
+`);
+
+let waku: Waku;
+
+export const initWaku = async() => {
+    waku = await Waku.create({ bootstrap: true });
+
+    const nodes = await getBootstrapNodes();
+    await Promise.all(nodes.map((addr) => waku.dial(addr)));
+
+    console.log("Waiting for peer...");
+    await waku.waitForConnectedPeer()
+
+
+    const processIncMsg = async (wakuMessage: { payloadAsUtf8: any; payload: any; }) => { 
+      if (!wakuMessage.payload) return;
+
+      const { timestamp, text } = proto.SimpleChatMessage.decode(
+        wakuMessage.payload
+      );
+
+      console.log(`Message Received: ${text}, sent at ${timestamp.toString()}`);
+    }
+    waku.relay.addObserver(processIncMsg, [ContentTopic])
+    console.log("init done");
 }
 
-export const ee = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
-}
-
-export const qq = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
-}
-export const bb = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
-}
-
-export const cc = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
-}
-
-
-export const pp = async (name: string): Promise<string> => {
-    let waku = await Waku.create({ bootstrap: true });
-    console.log(waku);
-    return `Hello ${name}`
-}
-
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
+export const sendMessage = async(message: string) => {
+    const payload = proto.SimpleChatMessage.encode({
+        timestamp: Date.now(),
+        text: message
+      });
+  
+      const wakuMessage = await WakuMessage.fromBytes(payload, ContentTopic);
+  
+      console.log("Sending payload");
+      await waku.relay.send(wakuMessage);
 }

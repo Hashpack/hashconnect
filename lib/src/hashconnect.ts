aad097ac92ad521863dd3ab7e05d797c802e31f8
Tyler Cote <tyler8cote@gmail.com>
2022-02-13 21:22:28 -0500

    Buffer polyfill (#58)
    * added buffer polyfill in lib
    
    * updated demo apps

--- lib/src/hashconnect.ts
+++ lib/src/hashconnect.ts
@@ -2,6 +2,7 @@ import { Event } from "ts-typed-events";
 import { IRelay, WebSocketRelay } from "./types/relay";
 import { MessageUtil, MessageHandler, MessageTypes, RelayMessage, RelayMessageType } from "./message"
 import { HashConnectTypes, IHashConnect } from "./types/hashconnect";
+global.Buffer = global.Buffer || require('buffer').Buffer;
 
 /**
  * Main interface with hashpack
